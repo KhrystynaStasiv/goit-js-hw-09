b87bd9cbbcca9bd2910dc5d585f426827bf09eb2
KhrystynaStasiv <khrystynalysanyk@gmail.com>
2024-06-25 22:25:47 +0300

    form-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,23 +1,42 @@
+'use strict';
+
+const LS_KEY = 'feedback-form-state';
 const formData = { email: '', message: '' };
 
-const form = document.querySelector('.feedback-form');
+const formElem = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name = "email"]');
 const messageInput = form.querySelector('textarea[name = "message"]');
 
-form.addEventListener('input', () => {
-  const formData = new FormData(form);
+formElem.addEventListener('input', () => {
+  const formData = new FormData(formElem);
   const email = formData.get('email');
   const message = formData.get('message');
   saveToLS('email', email);
   saveToLS('message', message);
-  saveToLS('formData', data);
 });
+
+formElem.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  e.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+  saveToLS('email', email);
+  saveToLS('message', message);
+  saveToLS('formData', data);
+
+  if (email === '' || message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log({ email, message });
+    form.reset();
+  }
+}
 
 function saveToLS(key, value) {
   const jsonData = JSON.stringify(value);
-  localStorage.setItem('feedback-form-state', jsonData);
+  localStorage.setItem(key, jsonData);
 }
-
 function loadFromLS(key) {
   const json = localStorage.getItem(key);
   try {
@@ -27,3 +46,5 @@ function loadFromLS(key) {
     return json;
   }
 }
+saveToLS();
+loadFromLS();
